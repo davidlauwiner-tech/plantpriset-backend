9661bd3272e722e0fb2bd528f536b95518404ff4
David Lauwiner <davidlauwiner@Mac.lan>
2026-03-16 10:52:01 +0100

    Force Python 3.11 with psycopg2

--- database.py
+++ database.py
@@ -1,9 +1,8 @@
 import os
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = os.environ.get("DATABASE_URL", "").replace("postgresql://", "postgresql+asyncpg://").replace("postgresql+pg8000://", "postgresql+asyncpg://")
+DATABASE_URL = os.environ.get("DATABASE_URL", "")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
