7fe42981327518dad5256ff066b3d36bf9f447ce
David Lauwiner <davidlauwiner@Mac.lan>
2026-03-16 10:45:30 +0100

    Switch to psycopg3

--- database.py
+++ database.py
@@ -1,19 +1,11 @@
 import os
-import ssl
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-DATABASE_URL = os.environ.get("DATABASE_URL", "")
+DATABASE_URL = os.environ.get("DATABASE_URL", "").replace("postgresql://", "postgresql+psycopg://")
 
-ssl_context = ssl.create_default_context()
-ssl_context.check_hostname = False
-ssl_context.verify_mode = ssl.CERT_NONE
-
-engine = create_engine(
-    DATABASE_URL,
-    connect_args={"ssl_context": ssl_context}
-)
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
